3a57e24207b5f7d1f4358e4a31e2861c256e5796
Bhavna Mehetre <bhavnamehetre3@gmail.com>
2026-05-25 11:50:31 +0530

    Add files via upload

---
+++ 5.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form method="post" action="5.1.php">
+            enter eno:<input type="text" name="e1"><br><br>
+            enter ename:<input type="text" name="e2"><br><br>
+            enter eaddr:<input type="text" name="e3"><br><br>
+            <input type="submit" value="submit">
+</form>
+</body>
+</html>
