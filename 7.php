eead2d0989ce04e42deb1c1cc6ae0af1be7a0244
Bhavna Mehetre <bhavnamehetre3@gmail.com>
2026-05-27 22:55:44 +0530

    Add files via upload

---
+++ 7.php
@@ -0,0 +1,10 @@
+<html>
+    <body>
+        <form method="post" action="7.1.php">
+            enter cust name:<input type="text" name="c1"><br><br>
+            enter cust address:<input type="text" name="c2"><br><br>
+            enter cust contact:<input type="text" name="c3"><br><br>
+            <input type="submit" valie="submit"><br>
+</form>
+</body>
+</html>
