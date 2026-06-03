4d3268e862a594abd4e1a3504869167ddaa2eb67
Bhavna Mehetre <bhavnamehetre3@gmail.com>
2026-06-03 18:52:06 +0530

    Add files via upload

---
+++ cookie2.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+
+        <form action="cookie3.php" method="post">
+            Enter emp no:<input type="text" name="e1"><br><br>
+            Enter emp name:<input type="text" name="e2"><br><br>
+            Enter emp address:<input type="text" name="e3"><br><br>
+            <input type="submit" value="submit">
+</form>
+</body>
+</html>
